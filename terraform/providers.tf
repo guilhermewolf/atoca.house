9abe1c217aabbe238afbaca9144fdf34bac2c0be
João Guilherme Wolf e Conte <guilhermewolf@outlook.com>
2024-05-15 19:32:43 +0200

    change atlantis vars names

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "4.32.0"
+    }
+  }
+}
+
 provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
--- terraform/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-cloudflare_api_token  = env("CF_API_KEY")
-zone_id               = env("CF_ZONE_ID")
-account_id            = env("CF_ACCOUNT_ID")
-domain                = env("CF_DOMAIN")

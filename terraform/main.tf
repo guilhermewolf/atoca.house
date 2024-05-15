37cd0ad2054bd0bef78fd7aeeccb2b8bb74b6c7a
João Guilherme Wolf e Conte <guilhermewolf@outlook.com>
2024-05-15 19:26:56 +0200

    test atlantis

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,10 +8,8 @@ terraform {
   }
 }
 
-resource "cloudflare_record" "test" {
-  zone_id = var.zone_id
-  name    = "test"
-  value   = "192.168.1.1"
-  type    = "CNAME"
-  proxied = true
+#create a output message saying test
+output "message" {
+  value = "Hello, World!"
 }
+

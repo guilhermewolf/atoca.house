d3d58ce02b3a8226f89a9b8e3b50745abfe7fe9a
João Guilherme Wolf e Conte <guilhermewolf@outlook.com>
2024-06-25 21:39:23 +0200

    add dns names for tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,3 @@
 terraform {
   backend "pg" {}
 }
-
-resource "cloudflare_record" "test" {
-  zone_id = var.zone_id
-  name    = "test"
-  value   = "192.168.1.1"
-  type    = "CNAME"
-  proxied = true
-}

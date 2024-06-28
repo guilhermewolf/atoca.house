09620bd0b7316d3f6ba03b8630f093b955453fec
João Guilherme Wolf e Conte <joao.wolf.e.conte@persgroep.net>
2024-06-28 15:21:20 +0200

    clenaup argocd

--- infra/argocd/test.sh
+++
@@ -1,6 +0,0 @@
-for resource in $(kubectl api-resources --verbs=list --namespaced -o name); do
-    for name in $(kubectl get $resource -n $NAMESPACE -o json | jq -r '.items[] | select(.metadata.finalizers) | .metadata.name'); do
-        echo "Patching $resource $name to remove finalizers..."
-        kubectl patch $resource $name -n $NAMESPACE -p '{"metadata":{"finalizers":[]}}' --type=merge
-    done
-done

44c32bdc9f9090b9629e37b2cbad31e770d07829
João Guilherme Wolf e Conte <joao.wolf.e.conte@persgroep.net>
2025-02-11 10:31:59 +0100

    add crunchy postgres r2 bucket

--- infra/terraform/r2.tf
+++ infra/terraform/r2.tf
@@ -3,3 +3,9 @@ resource "cloudflare_r2_bucket" "rpi_k8s" {
   name       = "rpi-k8s-backup"
   location   = "WEUR"
 }
+
+resource "cloudflare_r2_bucket" "rpi_k8s_crunchy_postgres" {
+  account_id = var.account_id
+  name       = "rpi-k8s-crunchy-postgres"
+  location   = "WEUR"
+}

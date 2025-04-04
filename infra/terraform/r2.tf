157c4783503f618c5f807b1260f9f7d1dc91161a
João Guilherme Wolf e Conte <joao.wolf.e.conte@persgroep.net>
2025-04-04 10:33:42 +0200

    rever cloudflare provider version

--- infra/terraform/r2.tf
+++ infra/terraform/r2.tf
@@ -2,10 +2,24 @@ resource "cloudflare_r2_bucket" "rpi_k8s" {
   account_id = var.account_id
   name       = "rpi-k8s-backup"
   location   = "WEUR"
+  lifecycle {
+    ignore_changes = [
+      jurisdiction,
+      location,
+      storage_class
+    ]
+  }
 }
 
 resource "cloudflare_r2_bucket" "rpi_k8s_crunchy_postgres" {
   account_id = var.account_id
   name       = "rpi-k8s-crunchy-postgres"
   location   = "WEUR"
+  lifecycle {
+    ignore_changes = [
+      jurisdiction,
+      location,
+      storage_class
+    ]
+  }
 }

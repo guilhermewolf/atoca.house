c59cb365cfd120a0317d10ec2281ee4600f0c85a
Guilherme Wolf <guilhermewolf@outlook.com>
2026-09-24 08:46:52 +0200

    add default site to unifi provider

--- infra/terraform/dns.tf
+++ infra/terraform/dns.tf
@@ -19,6 +19,7 @@ locals {
 resource "unifi_dns_record" "subdomain" {
   for_each    = toset(local.subdomains)
   name        = "${each.value}.${local.domain}"
+  site        = "default"
   enabled     = true
   record_type = "A"
   ttl         = "5m"
